eaca8889a36796b4c55f40c781a484df2b25c2b4
mmohiuddin08954 <mmohiuddin08954@ucumberlands.edu>
2024-09-02 01:29:15 +0300

    Add files via upload

---
+++ Insert_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort_decreasing(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key > arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# Example usage
+arr = [12, 11, 13, 5, 6]
+sorted_arr = insertion_sort_decreasing(arr)
+print("Sorted array in decreasing order:", sorted_arr)
